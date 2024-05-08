37fc4f3e2ac112e484c29a6e9062ef15e328b2a4
01001110J <a10100105555@gmail.com>
2024-05-08 16:51:46 -0500

    refactor: console message

---
+++ src/constants/index.js
@@ -0,0 +1,231 @@
+const rabbit = `          ,
+            /|      __
+           / |   ,-~ /
+          Y :|  //  /
+          | jj /( .^
+          >-"~"-v"
+         /       Y
+        jo  o    |
+       ( ~T~     j
+        >._-' _./
+       /   "~"  |
+      Y     _,  |
+     /| ;-"~ _  l
+    / l/ ,-"~    \\
+    \\//\\/      .- \\
+     Y        /    Y  
+     l       I     !
+     ]\\      _\\    /\\"
+    (" ~----( ~   Y.  )
+~~~~~~~~~~~~~~~~~~~~~~~~~`;
+
+const note = `    ;;;;;;;;;;;;;;;;;;; 
+;;;;;;;;;;;;;;;;;;;
+;                 ;
+;                 ;
+;                 ;
+;                 ;
+;                 ;
+;                 ;
+;                 ;
+,;;;;;            ,;;;;;
+;;;;;;            ;;;;;;
+\`;;;;'            \`;;;;'`;
+
+const flower = `         #       #       #
+##     ###     ##
+###   #####   ###
+#### ####### ####
+#################
+#################
+ ###############
+   ###########
+     #######
+       ***
+       ***
+       ***      ***
+       ***   *********
+       *****************
+       ***   *********
+       ***      ***
+       ***
+******      ***
+************* ***
+******    ***
+   ***
+  ***
+ ***
+***
+***
+***
+***
+***
+***
+*`;
+
+const batman = `       _,    _   _    ,_
+.o888P     Y8o8Y     Y888o.
+d88888      88888      88888b
+d888888b_  _d88888b_  _d888888b
+8888888888888888888888888888888
+8888888888888888888888888888888
+YJGS8P"Y888P"Y888P"Y888P"Y8888P
+Y888   '8'   Y8P   '8'   888Y
+'8o          V          o8'
+  \`                     \``;
+
+const cat = `M.					   .:M
+MMMM.					.:MMMM
+MMMMMMMM			     .:MMMMMMM
+:MMHHHMMMMHMM.	.:MMMMMMMMM:.	   .:MMHHMHMM:
+ :MMHHIIIHMMMM.:MMHHHHIIIHHMMMM. .:MMHIHIIHHM:
+  MMMHIIIIHHMMMIIHHMHHIIIIIHHMMMMMMHHHIIIIHHM:
+  :MMHIIIIIHMMMMMMMHHIIIIIIHHHMMMMMHHII:::IHM.
+   MH:I:::IHHMMMMMHHII:::IIHHMMMHHHMMM:I:IHMM
+   :MHI:HHIHMMHHIIHII::.::IIHMMHHIHHMMM::HMM:
+    MI::HHMMIIM:IIHII::..::HM:MHHII:::IHHMM:
+    MMMHII::..:::IHMMHHHMHHMMI:::...::IHM:
+    :MHHI::....::::HMMMMMMHHI::.. ..:::HM:
+     :MI:.:MH:.....:HMMMMHHMIHMMHHI:HH.:M
+     M:.I..MHHHHHMMMIHMMMMHMMHHHHHMMH:.:M.
+     M:.H..H  I:HM:MHMHI:IM:I:MM::  MMM:M:
+     :M:HM:.M I:MHMIIMMIIHM I:MM::.:MMI:M.
+     'M::MM:IMH:MMII MMHIMHI :M::IIHMM:MM
+      MH:HMMHIHMMMMMMHMMIMHIIHHHHIMMHHMM
+       MI:MMMMHI:::::IMM:MHI:::IMMMMHIM
+    :IMHIHMMMMMM:MMMMMHHHHMMMHI:M
+     HI:IMIHMMMM:MMMMMMHHHMI:.:M	  .....
+ ............M::..:HMMMMIMHIIHMMMMHII:M:::''''
+ ....:::MHI:.:HMMMMMMMMHHHMHHI::M:::::::''''''
+''   ...:MHI:.::MMHHHMHMIHMMMMHH.MI..........
+   ''  ...MHI::.::MHHHHIHHMM:::IHM           '''
+      '  IMH.::..::HMMHMMMH::..:HM:
+    :M:.H.IHMIIII::IIMHMMM:H.MH
+     IMMMH:HI:MMIMI:IHI:HIMIHM:
+       .MMI:.HIHMIMI:IHIHMMHIHI:MIM.
+      .MHI:::HHIIIIIHHI:IIII::::M:IM.
+     .MMHII:::IHIII::::::IIIIIIHMHIIM
+     MHHHI::.:IHHII:::.:::IIIIHMHIIHM:
+    MHHHII::..::MII::.. ..:IIIHHHII:IM.
+   .MHHII::....:MHII::.  .:IHHHI::IIHMM.
+   MMHHII::.....:IHMI:. ..:IHII::..:HHMM
+   MHHII:::......:IIHI...:IHI::.....::HM:
+  :MMH:::........ ...::..::....  ...:IHMM
+  IMHIII:::..........	  .........::IHMM.
+  :MHIII::::......	    .......::IHMM:
+   MHHIII::::...	     ......::IHMM:
+   IMHHIII:::...	     .....::IIHMM,
+   :MHHIII:::I:::...	 ....:::I:::IIHMM
+    MMHHIII::IHI:::...........:::IIH:IHMM
+    :MMHHII:IIHHI::::::.....:::::IH:IHMIM
+     MMMHHII:IIHHI:::::::::::::IHI:IIHM:M.
+     MMMHHIII::IHHII:::::::::IHI:IIIHMM:M:
+     :MMHHHIII::IIIHHII::::IHI..IIIHHM:MHM
+     :MMMHHII:..:::IHHMMHHHHI:IIIIHHMM:MIM
+     .MMMMHHII::.:IHHMM:::IIIIIIHHHMM:MI.M
+   .MMMMHHII::.:IHHMM:::IIIIIIHHHMM:MI.M
+ .MMMMHHMHHII:::IHHMM:::IIIIIHHHHMM:MI.IM.
+.MMHMMMHHHII::::IHHMM::I&&&IHHHHMM:MMH::IM.
+   .MMHHMHMHHII:::.::IHMM::IIIIHHHMMMM:MMH::IHM
+   :MHIIIHMMHHHII:::IIHMM::IIIHHMMMMM::MMMMHHHMM.
+   MMHI:IIHMMHHHI::::IHMM:IIIIHHHMMMM:MMMHI::IHMM.
+   MMH:::IHMMHHHHI:::IHMM:IIIHHHHMMMM:MMHI:.:IHHMM.
+   :MHI:::IHMHMHHII::IHMM:IIIHHHMMMMM:MHH::.::IHHM:
+   'MHHI::IHMMHMHHII:IHMM:IIHHHHMMMM:MMHI:...:IHHMM.
+:MHII:IIHMHIHHIIIIHMM:IIHHHHMMMM:MHHI:...:IIHMM:
+'MHIII:IHHMIHHHIIHHHMM:IHHHMMMMM:MHHI:..::IIHHM:
+ :MHHIIIHHMIIHHHIHHHMM:HHHHMMMMM:MHII::::IIIHHMM
+  MHHIIIIHMMIHHHIIHHMM:HHHHMMMM:MMHHIIHIIIIIHHMM.
+  'MHHIIIHHMIIHHIIIHMM:HHHMMMMH:MHHMHII:IIIHHHMM:
+   'MHHIIIHMMIHHHIHHMM:HHHMMMHH:MMIMMMHHHIIIHHMM:
+    'MHHIIHHMIHHHHHMMM:HHHMMMH:MIMMMMMMMMMMHIHHM:
+     'MHIIIHMMIHHHHHMM:HHHMMMH:IMMMMMHHIHHHMMHHM'
+      :MHHIIHMIHHHHHMM:HHHMMMM:MMHMMHIHMHI:IHHHM
+       MHHIIHM:HHHHHMM:HHHMMMM:MMMHIHHIHMM:HHIHM
+    MHHIHM:IHHHHMM:HHHHMM:MMHMIIHMIMMMHMHIM:
+    :MHIHMH:HHHHMM:HHHHMM:MMHIIHMIIHHMMHIHM:
+     MMHHMH:HHHHMM:HHHHMM:MHHIHMMIIIMMMIIHM'
+     'MMMMH:HHHHMM:HHHMM:MHHHIMMHIIII::IHM:
+      :MMHM:HHHHMM:HHHMM:MHIHIMMHHIIIIIHM:
+       MMMM:HHHHMM:HHHHM:MHHMIMMMHHHIHHM:MMMM.
+       :MMM:IHHHMM:HHHMM:MHHMIIMMMHHMM:MMMMMMM:
+       :MMM:IHHHM:HHHHMM:MMHHHIHHMMM:MMMMMMMMMM
+        MHM:IHHHM:HHHMMM:MMHHHHIIIMMIIMMMMMMMMM
+        MHM:HHHHM:HHHMMM:HMMHHHHHHHHHMMMMMMMMM:
+     .MI:MM:MHHMM:MHMMHMHHMMMMHHHHHHHMMMMMMMMM'
+    :IM:MMIM:M:MM:MH:MM:MH:MMMMMHHHHHMMMMMMMM'
+    :IM:M:IM:M:HM:IMIHM:IMI:MMMMMHHHMMMMMM:'
+     'M:MHM:HM:MN:HMIHM::M'   '::MMMMMMM:'
+        'M'HMM'M''M''HM'I'
+
+`;
+
+const dragon = `
+             __.-/|
+             \`o_O'
+              =( )=  +-----+
+                U|   | BEN |
+      /\\  /\\   / |   +-----+
+     ) /^\\) ^\\/ _)\\     |
+     )   /^\\/   _) \\    |
+     )   _ /  / _)  \\___|_
+ /\\  )/\\/ ||  | )_)\\___,|))
+<  >      |(,,) )__)    |
+ ||      /    \\)___)\\)
+ | \\____(      )___) )____
+  \\______(_______;;;)__;;;)
+`;
+
+const skeleton = `           .7
+.'/        
+/ /        
+/ /        
+/ /        
+/ /        
+/ /        
+/ /        
+/ /                 
+/ /                  
+__|/                   
+,-\\__\\
+|f-"Y\\|
+\\()7L/
+cgD                            __ _
+|\\(                          .'  Y '>,
+\\ \\                        / _   _   \\
+\\\\\\                       )(_) (_)(|}
+\\\\\\                      {  4A   } /
+\\\\\\                      \\uLuJJ/\\l
+\\\\\\                     |3    p)/
+\\\\\\___ __________      /nnm_n//
+c7___-__,__-),__)(".  \\_>-<_/D
+      //V     \\_"-._.__G G_c__.-__<"/ ( \\
+             <"-._>__-,G_.___)\   \\7\\
+            ("-.__.| \\ "<.__.-" )   \\ \\
+            |"-.__"\\  |"-.__.-".\\   \\ \\
+            ("-.__""|!|"-.__.-".)    \\_\\
+            \\ "-.__"\\_|"-.__.-"./      \\ l
+              "-.__""">G>-.__.-">       .--,_
+                  ""  G`;
+
+const unicorn =
+  "\\.\n \\      .\n  \\ _,.+;)_\n  .\\;~%:88%%.\n (( a   `)9,8;%.\n /`   _) ' `9%%%?\n(' .-' j    '8%%'\n `\"+   |    .88%)+._____..,,_   ,+%$%.\n       :.   d%9`             `-%*\"'~%$.\n    ___(   (%C                 `.   68%%9\n  .\"        \\7                  ;  C8%%)`\n  : .\"-.__,'.____________..,`   L.  \\86' ,\n  : L    : :            `  .'\\.   '.  %$9%)\n  ;  -.  : |             \\  \\  \"-._ `. `~\"\n   `. !  : |              )  >     \". ?\n     `'  : |            .' .'       : |\n         ; !          .' .'         : |\n        ,' ;         ' .'           ; (\n       .  (         j  (            `  \\";
+
+const cown = `        \\   ^__^
+\\  (oo)\\_______
+   (__)\\       )\\/\\
+       ||----w |
+       ||     ||  Moo!`;
+
+
+export const figures = [
+  rabbit,
+  note,
+  flower,
+  batman,
+  cat,
+  dragon,
+  skeleton,
+  unicorn,
+  cown
+];
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import http from 'http';
 import reload from 'reload';
+import chalk from 'chalk';
+import terminalLink from 'terminal-link';
 
 import app from './config/server.js';
+import { figures } from './constants/index.js';
 
 const server = http.createServer(app);
 server.listen(app.get('port'), () => {
-  console.log('Is up');
+  console.log('');
+  console.log('');
+  console.log('');
+  console.log('');
+  console.log(figures[Math.floor(Math.random() * figures.length)]);
+  console.log('');
+  console.log('');
+  console.log('');
+  console.log(chalk.yellow('|============================================|'));
+  console.log(chalk.yellow('|                                            |'));
+  console.log(terminalLink('| Running on: ', `http://localhost:${app.get('port')}`));
+  console.log(chalk.yellow('|                                            |'));
+  console.log(chalk.yellow('|============================================|'));
 });
 
 reload(app);
