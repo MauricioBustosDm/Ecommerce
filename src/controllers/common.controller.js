d025ea08816896e9c854c5038a400364dd70688e
01001110J <a10100105555@gmail.com>
2024-05-08 17:33:52 -0500

    feat: Favicon

--- src/controllers/common.controller.js
+++ src/controllers/common.controller.js
@@ -1,9 +1,9 @@
 const homeController = (req, res) => {
-  res.render('pages/home');
+  res.render('pages/home', { title: 'Home' });
 };
 
 const aboutController = (req, res) => {
-  res.render('pages/about');
+  res.render('pages/about', { title: 'About' });
 };
 
 export { homeController, aboutController };
